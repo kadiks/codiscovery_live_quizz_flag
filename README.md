aa5e70dd82421fa8ee8a7323688695ad1b675fc9
Jénaïc Cambré <me@jenaiccambre.com>
2020-04-24 12:07:56 +0200

    commit after live

---
+++ README.md
@@ -0,0 +1,16 @@
+# Quizz Guess the flag
+
+## TODO
+
+- [x] Manage screens (question + answer + end)
+- [x] Create a question
+- [x] Manage click on answer card
+- [ ] Style it up!
+
+### Optional
+
+- [ ] Remove parenthesis and commas
+
+## Resources
+
+- [https://restcountries.eu/](https://restcountries.eu/)
---
+++ js/generateQuestion.js
@@ -0,0 +1,27 @@
+const generateQuestion = (countries) => {
+    const random = 200; // parseInt(Math.random() * countries.length);
+
+    const country = countries[random];
+
+    const possibilities = [];
+    for (let index = 0; index < 3; index++) {
+        const r = parseInt(Math.random() * countries.length);
+        const name = countries[r].name;
+        possibilities.push(name);
+    }
+    possibilities.push(country.name);
+
+    possibilities.sort((a, b) => {
+        return a.charCodeAt(0) - b.charCodeAt(0);
+    });
+
+    const question = {
+        flag: country.flag,
+        answer: country.name,
+        possibilities
+    };
+
+    console.log(question);
+
+    return question;
+};
